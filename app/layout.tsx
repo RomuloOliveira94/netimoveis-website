d1865a13ff38bfd0a0c2d1afc3ac70120a0911a9
Rômulo Augusto <romuloffall@gmail.com>
2024-05-07 11:55:45 -0300

    #setup commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter, Source_Sans_3 } from "next/font/google";
+import "./globals.css";
+import Header from "./components/header";
+
+const inter = Source_Sans_3({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Netimóveis",
+  description: "Vende. Aluga. Conecta",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
